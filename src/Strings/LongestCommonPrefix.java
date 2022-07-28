c34294d36dfdca833441385b23a249d98f540e6a
kmts571011@Sai2 <mohanapriyanarapareddy@localhost>
2022-07-28 18:05:28 -0500

    error fix

--- src/Strings/LongestCommonPrefix.java
+++ src/Strings/LongestCommonPrefix.java
@@ -17,10 +17,10 @@ System.out.println(longestCommonPrefix(strs));
 		}
 		String prefix=strs[0];
 		for(int i=0;i<strs.length;i++)
-		{while(strs[i].indexOf(prefix)!=0)
+		{
+			while(strs[i].indexOf(prefix)!=0)
 			prefix=prefix.substring(0, prefix.length()-1);
-		if(prefix.isEmpty())return "";
-			
+		if(prefix.isEmpty())return "";			
 		}return prefix;
 	}
 

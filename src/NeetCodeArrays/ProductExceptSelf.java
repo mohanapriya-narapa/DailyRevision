c6e5662a176b22d6449a05f6c45fae4a3e0c86c6
mohanaTimeline <mohanapriya.narapa@outlook.com>
2023-10-22 19:49:02 -0500

    product except itself

---
+++ src/NeetCodeArrays/ProductExceptSelf.java
@@ -0,0 +1,30 @@
+package NeetCodeArrays;
+
+public class ProductExceptSelf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int[] nums=new int[] {1,2,3,4};
+int[] left=new int[nums.length];
+int[] right=new int[nums.length];
+int[] prod=new int[nums.length];
+left[0]=1;
+right[nums.length-1]=1;
+for(int i=1;i<nums.length;i++)
+{
+	left[i]=left[i-1]*nums[i-1];
+}
+for(int i=nums.length-2;i>=0;i--)
+{
+	right[i]=right[i+1]*nums[i+1];
+}
+for(int i=0;i<nums.length;i++)
+{
+	prod[i]=left[i]*right[i];
+}
+for(int x:prod)
+{
+	System.out.println(x);
+}
+	}
+}

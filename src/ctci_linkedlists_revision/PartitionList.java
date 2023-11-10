751b6c1b0bda354d6c8d8c1ee8a8350d52adcebc
mohanaTimeline <mohanapriya.narapa@outlook.com>
2023-11-10 17:35:23 -0600

    partition list

---
+++ src/ctci_linkedlists_revision/PartitionList.java
@@ -0,0 +1,60 @@
+package ctci_linkedlists_revision;
+
+import ctci_linkedlists.PartitionLists.Node;
+
+public class PartitionList {
+	public static class Node
+	{
+		int data;
+		Node next;
+		Node(int data)
+		{
+			this.data=data;
+			this.next=next;
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Node first=new Node(10);
+		Node second=new Node(12);
+		Node third=new Node(3);
+		Node fourth=new Node(5);
+		Node fifth=new Node(2);
+		Node sixth=new Node(9);
+		first.next=second;
+		second.next=third;
+		third.next=fourth;
+		fourth.next=fifth;
+		fifth.next=sixth;
+
+
+		Node result =partitionList(first,5);
+		print(result);
+	}
+	private static void print(Node result) {
+		// TODO Auto-generated method stub
+		
+	}
+	private static Node partitionList(Node n, int x) {
+		// TODO Auto-generated method stub
+		Node head=n;
+		Node tail=n;
+		while(n!=null)
+		{
+			Node next=n.next;
+			if(n.data<x)
+			{
+				n.next=head;
+				n=head;
+			}
+			else
+			{
+				tail.next=n;
+				n=tail;
+			}
+			n=next;
+		}
+		return head;
+	}
+
+}

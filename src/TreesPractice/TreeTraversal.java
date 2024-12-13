614a632907945202f8db9ba2f650184ad8bbebf4
mohanapriya-narapa <mohanapriya.narapa@outlook.com>
2024-12-13 00:15:03 -0600

    'treeTraversal'

---
+++ src/TreesPractice/TreeTraversal.java
@@ -0,0 +1,20 @@
+package TreesPractice;
+
+public class TreeTraversal {
+public class Node{
+	public int data;
+	public Node left;
+	public Node right;
+	Node(int data)
+	{
+		this.data=data;
+		this.left=null;
+		this.right=null;
+	}
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

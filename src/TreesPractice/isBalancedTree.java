25845dc1637905f602f1f3bb8d64e585db547922
mohanapriya-narapa <mohanapriya.narapa@outlook.com>
2024-12-14 10:37:19 -0600

    'balancedTree'

---
+++ src/TreesPractice/isBalancedTree.java
@@ -0,0 +1,45 @@
+package TreesPractice;
+
+public class isBalancedTree {
+	public static class TreeNode
+	{
+		public int data;
+		public TreeNode left;
+		public TreeNode right;
+		public TreeNode(int data)
+		{
+			this.data=data;
+			this.left=null;
+			this.right=null;
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TreeNode root=new TreeNode(3);
+		TreeNode left=new TreeNode(4);
+		TreeNode right=new TreeNode(5);
+		TreeNode leftchild1=new TreeNode(1);
+		TreeNode leftchild2=new TreeNode(2);
+		root.left=left;
+		root.right=right;
+		left.left=leftchild1;
+		left.right=leftchild2;
+		System.out.println(isBalancedTree(root));
+	}
+	private static boolean isBalancedTree(TreeNode root) {
+		// TODO Auto-generated method stub
+		if(root==null)
+			return true;
+		int lh=height(root.left);
+		int rh=height(root.right);
+		return Math.abs(lh-rh)<=1 && isBalancedTree(root.left) && isBalancedTree(root.right);
+	}
+	private static int height(TreeNode root) {
+		// TODO Auto-generated method stub
+		if(root==null)
+			return 0;
+		return Math.max(height(root.left) , height(root.right))+1;
+		
+	}
+
+}
